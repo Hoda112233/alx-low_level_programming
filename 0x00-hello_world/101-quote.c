c4abdda61a0ec6d8e3d2a742123a19a83865c07c
HODA <hodasalehbelal@gmail.com>
2023-08-10 15:55:12 -0700

    hh

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,17 @@
 #include <unistd.h>
 
 /**
- * main - Entry Point
+ * main - Entry point
  *
- * Description: print code
+ * Description: c prog
  *
  * Return: 1 (fail)
 */
 
 int main(void)
 {
-	char que[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, que, 59);
+	write(1, quo, 59);
 	return (1);
 }
