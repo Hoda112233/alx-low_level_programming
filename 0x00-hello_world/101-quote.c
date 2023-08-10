95e2558f819b8f9f4391e636bbb3c17b3d45c2c6
HODA <hodasalehbelal@gmail.com>
2023-08-10 15:34:58 -0700

    hh

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: print code
+ *
+ * Return: 1 (fail)
+*/
+
+int main(void)
+{
+	char que[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+
+	write(1, que, 59);
+	return (1);
+}
