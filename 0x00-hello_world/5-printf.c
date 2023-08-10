f58f95a72cddc88ee353dd347f6cf2bef76ab879
HODA <hodasalehbelal@gmail.com>
2023-08-10 13:28:03 -0700

    hh

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,8 +6,9 @@
  *
  * Return: Alaways 0 (Success)
 */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
