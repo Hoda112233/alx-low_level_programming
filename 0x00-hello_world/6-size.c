ff6dac0baba40ea757b28760b7aad96447c36727
HODA <hodasalehbelal@gmail.com>
2023-08-10 15:09:13 -0700

    hh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,16 +3,17 @@
 /**
  * main - Entry point
  *
- * Description: A C program using sizeof
+ * Description: C prog sizeof
  *
  * Return: Alaways 0 (Success)
 */
 
 int main(void)
 {
-	printf("size of a chart: %u byte(s)\n", sizeof(char));
-	printf("size of an int: %u byte(s)\n", sizeof(int));
-	printf("size of a long  int: %u byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
 	return (0);
 }
