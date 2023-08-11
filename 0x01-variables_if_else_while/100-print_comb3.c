efaa26593bc36173e78c85e6dfe6198951a361a4
HODA <hodasalehbelal@gmail.com>
2023-08-11 15:44:19 -0700

    hh

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible different combinations
+ * of two digits separated by ', '.
+ * Numbers should be printed in ascending order with two digits.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int first_digit;
+	int second_digit;
+
+	for (first_digit = 0; first_digit < 9; first_digit++)
+	{
+		for (second_digit = first_digit + 1; second_digit <= 9; second_digit++)
+		{
+			putchar(first_digit + '0');
+			putchar(second_digit + '0');
+			if (first_digit != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
