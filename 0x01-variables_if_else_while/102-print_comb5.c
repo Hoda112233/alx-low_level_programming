086abf6ce6b1476d3eac4902ee3cc31e69ae689b
HODA <hodasalehbelal@gmail.com>
2023-08-11 16:46:10 -0700

    hh

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,8 +5,9 @@
  *
  * Description: Prints all possible combinations
  * of two two-digit numbers separated by comma and space.
+ * Numbers should be printed in ascending order with two digits.
  *
- * Return: 0 (Success)
+ * Return: 0
  */
 
 int main(void)
@@ -15,24 +16,21 @@ int main(void)
 
 	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (num2 = 0; num2 <= 99; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			if (num1 <= num2)
-			{
-				putchar((num1 / 10) + '0');
-				putchar((num1 % 10) + '0');
-				putchar(' ');
-				putchar((num2 / 10) + '0');
-				putchar((num2 % 10) + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-				if (num1 != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+			if (num1 != 98 || num2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
 	putchar('\n');
-	return (0);
+return (0);
 }
