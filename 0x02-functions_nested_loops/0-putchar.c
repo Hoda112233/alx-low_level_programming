bb64b38fdbb3b5ca79e3a57db25708e3b3aecf36
HODA <hodasalehbelal@gmail.com>
2023-08-15 14:34:26 -0700

    HHII

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.c"
+
+/**
+ * main - starting
+ *
+ * Description: a code that prints _putchar
+ * followed by new line using C language
+ *
+ * Return: 0 if succeed
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+			_putchar('\n');
+	return (0);
