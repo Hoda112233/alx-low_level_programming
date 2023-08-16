162361cc98c1cc7bb1a2be9f239cee9a6a080610
HODA <hodasalehbelal@gmail.com>
2023-08-16 13:43:32 -0700

    h

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,11 +4,12 @@
  * print_alphabet_10_times - Prints the lowercase alphabet 10 times,
  *                          each followed by a new line.
  */
+
 void print_alphabet_10_times(void)
 {
 	int ch, repeat;
 
-	for (repeat = 0; repeat < 10; repeat++)
+	for (repeat = 0; repeat <= 9; repeat++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
