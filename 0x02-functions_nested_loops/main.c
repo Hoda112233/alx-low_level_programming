1a4b7a3fe4cc21303440ec8a85e558344f34bd11
HODA <hodasalehbelal@gmail.com>
2023-08-20 03:02:26 -0700

    hhh

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/*
+ * main : Code
+ *
+ * Description: This file contains the main function for the program that
+ *              prints the alphabet 10 times in lowercase, each followed
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
