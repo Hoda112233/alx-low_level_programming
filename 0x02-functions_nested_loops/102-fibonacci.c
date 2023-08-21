45a68100e01a96b557cf5e455d5364af56eb3397
HODA <hodasalehbelal@gmail.com>
2023-08-21 14:21:00 -0700

    hhh

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Generates and prints the first 50 Fibonacci nums,
+ * starting with 1 and 2. Shows the sequence and its calculation
+ * using standard library.
+ * Return: Alaways 0 (success)
+*/
+
+int main(void)
+{
+	unsigned long long int fib1 = 1, fib2 = 2, nextFib;
+
+	printf("%llu, %llu", fib1, fib2);
+
+	for (int i = 2; i < 50; i++)
+	{
+		nextFib = fib1 + fib2;
+		printf(", %llu", nextFib);
+
+		fib1 = fib2;
+		fib2 = nextFib;
+
+	}
+	printf("\n");
+	return (0);
+}
