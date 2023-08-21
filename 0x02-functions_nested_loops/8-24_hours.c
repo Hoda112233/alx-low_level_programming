0e2b0d7d6b047c8e8d23335fc36dcc73de5d8020
HODA <hodasalehbelal@gmail.com>
2023-08-21 06:20:33 -0700

    HODA

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,27 @@
 #include "main.h"
 
 /**
- * Description: jack_bauer- a function that prints words every minute
- * of the day
-*/
+ * jack_bauer - A function that prints the time every minute of the day.
+ *
+ * Description: This function iterates
+ * through all the minutes of a day
+ * (24 hours) and prints the time
+ * in the format "HH:MM" for each minute.
+ */
 
 void jack_bauer(void)
 {
 	int min, hr;
 
-	for (hr = 0; hr <= 24; hr++)
+	for (hr = 0; hr < 24; hr++)
 	{
-		for (min = 0, min <= 59, min++)
+		for (min = 0; min < 60; min++)
 		{
-			_putchar((hr / 10) + 48);
-			_putchar((hr % 10) + 48);
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
 			_putchar(':');
-			_putchar((min / 10) + 48);
-			_putchar((min % 10) + 48);
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
 			_putchar('\n');
 		}
 	}
