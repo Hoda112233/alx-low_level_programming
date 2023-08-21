c4c12afce5689751b5f0999278eb5163262cd301
HODA <hodasalehbelal@gmail.com>
2023-08-21 15:19:52 -0700

    hhh

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,32 +1,72 @@
 #include "main.h"
 
 /**
+ * numLength - Returns the length of a number in digits.
+ *
+ * @num: The number for which to find the length.
+ *
+ * Return: The number of digits in the given number.
+ */
+
+int numLength(int num)
+{
+	int length = 0;
+
+	if (num == 0)
+		return (1);
+
+	while (num)
+	{
+		num = num / 10;
+		length++;
+	}
+	return (length);
+}
+
+/**
  * main - Entry point
  *
- * Description: Generates and prints the first 98 Fibonacci numbers,
- * starting with 1 and 2, followed by a new line. Numbers are separated
- * by a comma and space. The program adheres to the specified constraints,
- * utilizing the standard library without using long long, malloc,
- * pointers, arrays/tables, or structures.
+ * Description: Prints the first 98 Fibonacci numbers starting with 1 and 2,
+ *              followed by a new line.
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	unsigned long fib1 = 1, fib2 = 2, nextFib;
-	int count;
+	int count, initial0s;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, flo = 0, f20 = 0, sumo = 0;
 
-	printf("%.0f, %.0f", fib1, fib2);
-
-	for (count = 2; count < 98; count++)
+	for (count = 1; count <= 98; count++)
 	{
-		nextFib = fib1 + fib2;
-		printf(", %.0f", nextFib);
+		if (flo > 0)
+		{
+			printf("%lu", flo);
+			initial0s = numLength(mx) - numLength(f1);
+			while (flo > 0 && initial0s > 0)
+			{
+				printf("0");
+				initial0s--;
+			}
+			printf("%lu", f1);
+		}
+		else
+		{
+			printf("%lu", f1);
+		}
+		sum = (f1 + f2) % mx;
+		sumo = flo + f20 + (f1 + f2) / mx;
 
-		fib1 = fib2;
-		fib2 = nextFib;
+		f2 = sum;
+		f1 = f2;
+		flo = f20;
+		f20 = sumo;
+
+		if (count == 98)
+			printf(", ");
+		else
+			printf("\n");
+
 	}
-	printf("\n");
 	return (0);
 }
