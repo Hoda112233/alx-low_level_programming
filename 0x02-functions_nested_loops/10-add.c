32730ed1c41ae226d808542329d502079fa797d4
HODA <hodasalehbelal@gmail.com>
2023-08-21 09:58:46 -0700

    hh

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - function that adds 2 integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a + b
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
