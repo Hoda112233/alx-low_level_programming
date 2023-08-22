3a1781cad8301d375c441429d680ae8d2d8c4c41
HODA <hodasalehbelal@gmail.com>
2023-08-22 10:39:23 -0700

    hhh

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_rev - Prints a string in reverse followed by a new line.
@@ -18,5 +17,5 @@ void print_rev(char *s)
 	{
 		putchar(s[i]);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
