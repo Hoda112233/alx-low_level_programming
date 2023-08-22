46bfe2680119fb94690d60996b46d4d341b6fa8f
HODA <hodasalehbelal@gmail.com>
2023-08-22 08:59:21 -0700

    hhh

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,13 @@
 
 void print_rev(char *s)
 {
-	int length = 0;
-	int i;
+	int i = 0;
 
-	while (s[length] != '\0')
+	while (s[i])
 	{
-		length++;
+		i++;
 	}
-	for (i = length - 1; i >= 0; i--)
+	while (i--)
 	{
 		_putchar(s[i]);
 	}
