53fec15f362f2f9a7589fe7c864950d0e3b4d659
HODA <hodasalehbelal@gmail.com>
2023-08-26 13:39:13 -0700

    hhh

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,26 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * multiply - Multiplies two integers.
+ * multiply - Multiplies two integers
  *
- * @a: The first integer.
- * @b: The second integer.
+ * @a: The first integer
+ * @b: The second integer
  *
- * Return: The product of the two integers.
+ * Return: The product of the two integers
 */
 
 int multiply(int a, int b)
-
 {
-return (a * b);
-}
-
-int main(void)
-{
-	int num1 = 5;
-	int num2 = 7;
-	int result = multiply(num1, num2);
-printf("The product of %d and %d is %d\n", num1, num2, result);
-return (0);
+	return (a * b);
 }
