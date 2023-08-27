294a89a60f85dd5a3bd4cded0f31282befe25bf1
HODA <hodasalehbelal@gmail.com>
2023-08-27 01:57:12 -0700

    hhh

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,17 +9,20 @@
 
 void print_diagonal(int n)
 {
+	if (n <= 0)
+	{
+		_putchar('\n');
+	return;
+	}
+
 	int p, s;
 
-	if (n <= 0)
-		_putchar('\n');
-	else
+	for (p = 0; p <= n; p++)
 	{
-		for (p = 1; p <= n; p++)
 		{
-			for (s = 1; s <= p; s++)
+			for (s = 0; s <= p; s++)
 				_putchar(' ');
-			_putchar(92);
+			_putchar('\\');
 			_putchar('\n');
 		}
 	}
