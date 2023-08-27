5c4e805e7c31eaed31943e47cf2fdbe0da36902b
HODA <hodasalehbelal@gmail.com>
2023-08-27 14:00:54 -0700

    hhh

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,11 +14,11 @@ char *_strcat(char *dest, char *src)
 	int c1, c2;
 
 	c1 = 0;
-	while (dest[c])
-		c++;
+	while (dest[c1])
+		c1++;
 
 	for (c2 = 0; src[c2]; c2++)
-		dest[c++] = src[c2];
+		dest[c1++] = src[c2];
 
 	return (dest);
 }
