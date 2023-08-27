fa3378b96ef711e8957ef0c800959312555347f3
HODA <hodasalehbelal@gmail.com>
2023-08-27 14:17:40 -0700

    hhh

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ *
+ * @s1: Pointer to the first string
+ * @s2: Pointer to the second string
+ *
+ * Return: 0 if the strings are equal
+ * a positive value if s1 > s2
+ * a negative value if s1 < s2
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+
+		s1++;
+		s2++;
+	}
+
+	if (*s1 != *s2)
+		return (*s1 - *s2);
+
+	return (0);
+}
