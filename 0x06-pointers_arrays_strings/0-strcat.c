c5d15f15694ae7d1ae866d9d0091948763a6d3eb
HODA <hodasalehbelal@gmail.com>
2023-08-27 13:57:23 -0700

    hhh

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ *
+ * @dest: Pointer to the destination string
+ * @src: Pointer to the source string
+ *
+ * Return: Pointer to the resulting string dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int c1, c2;
+
+	c1 = 0;
+	while (dest[c])
+		c++;
+
+	for (c2 = 0; src[c2]; c2++)
+		dest[c++] = src[c2];
+
+	return (dest);
+}
