9e18dad24127c6c9dbf47fc12fd6303350723df6
HODA <hodasalehbelal@gmail.com>
2023-08-27 03:11:34 -0700

    hhh

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,19 +2,19 @@
 
 /**
  * puts2 - Prints every other character of a string
- * starting with the first character.
+ * starting with the first character
  *
- * @str: The string to be printed.
- */
+ * @str: The string to be printed
+*/
+
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; ++i)
 	{
-		_putchar(str[i]);
-
-		i += 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
