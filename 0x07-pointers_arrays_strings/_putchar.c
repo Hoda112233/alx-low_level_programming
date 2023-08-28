00b12af71ca33d3673fbfd4f0d501aa07fd1f3a2
HODA <hodasalehbelal@gmail.com>
2023-08-28 14:22:39 -0700

    hhh

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#includie <unistd.h>
+
+/**
+ * _putchar - Writes a character to stdout.
+ *
+ * @c: The character to be written.
+ *
+ * Return: On success, returns the number of characters written.
+ *         On error, -1 is returned.
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
