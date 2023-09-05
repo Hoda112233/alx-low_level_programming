617284c13d2544cc74eab5b078cf86ba4d20b6a3
HODA <hodasalehbelal@gmail.com>
2023-09-05 03:32:13 -0700

    hhh

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer to newly allocated memory containing concatenated string
+ *         or NULL on failure
+ */
+char *str_concat(char *s1, char *s2)
+{
+char *concatenated;
+int len1 = 0, len2 = 0, i = 0;
+
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+
+len1 = _strlen(s1);
+len2 = _strlen(s2);
+concatenated = malloc((len1 + len2 + 1) * sizeof(char));
+
+if (concatenated == NULL)
+return (NULL);
+
+for (i = 0; i < len1; i++)
+concatenated[i] = s1[i];
+
+for (i = 0; i < len2; i++)
+concatenated[len1 + i] = s2[i];
+
+concatenated[len1 + len2] = '\0';
+
+return (concatenated);
+}
