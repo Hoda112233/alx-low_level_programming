987099ec68ca5235ca9178ff497d31be5249c0bf
HODA <hodasalehbelal@gmail.com>
2023-09-08 12:05:20 -0700

    hhh

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,29 +15,24 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 void *new_ptr;
 
-if (ptr == NULL)
-{
-new_ptr = malloc(new_size);
-if (new_ptr == NULL)
-return (NULL);
-return (new_ptr);
-}
-
-if (new_size == 0)
+if (new_size == 0 && ptr != NULL)
 {
 free(ptr);
 return (NULL);
 }
 
-if (new_size == old_size)
+if (ptr == NULL)
+{
+new_ptr = malloc(new_size);
+return (new_ptr);
+}
+
+if (new_size <= old_size)
 return (ptr);
 
 new_ptr = malloc(new_size);
 if (new_ptr == NULL)
 return (NULL);
-
-if (new_size < old_size)
-old_size = new_size;
 
 for (unsigned int i = 0; i < old_size; i++)
 ((char *)new_ptr)[i] = ((char *)ptr)[i];
