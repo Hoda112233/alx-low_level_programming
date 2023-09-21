f33cb36ce83598f1e9e473810e2cede35e6a36e6
TEAM <hodasalehbelal@gmail.com>
2023-09-21 14:45:06 -0700

    hhh

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: sring
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - prints linked list
+ * @h: pointer
+ *
+ * Return: size
+ */
+size_t print_list(const list_h *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
