6d4923362793f2f4630469c2684c50139bbefc2f
HODA <hodasalehbelal@gmail.com>
2023-09-21 15:03:55 -0700

    hhh

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,9 +7,10 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string
- * @len: lenght
- * @next: next node
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
  * Description: singly linked list node structure
  */
 typedef struct list_s
@@ -19,6 +20,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 int _putchar(char c);
 int _strlen(char *s);
 size_t print_list(const list_t *h);
