e81d8524c7ead4568a2e7e7ea072b3884b2944e8
TEAM <hodasalehbelal@gmail.com>
2023-09-24 03:27:10 -0700

    hhh

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string to the standard output followed by a newline.
+ * @str: The string to be printed.
+ *
+ * Description:
+ * This function takes a pointer to a string and prints it to the standard
+ * output followed by a newline character.
+*/
+void _puts(char *str);
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ * This is the main function, which serves as the entry point of the program.
+ * It prints a message to the standard output.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+_puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
